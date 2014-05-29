7da3e305cd7bc20cc37a1edc9421d12edeacc78a
Joel Niklaus <j.niklaus.95@gmail.com>
2014-05-29 11:58:10 +0200

    git repository with work already done initialized

---
+++ src/problems/Problem007.java
@@ -0,0 +1,41 @@
+package problems;
+
+import java.util.ArrayList;
+
+public class Problem007 {
+
+	static int limit = 10001;
+	static ArrayList<Integer> primes = new ArrayList<Integer>();
+	
+	public static void main(String[] args) {
+		Timer timer = new Timer();
+		calculatePrimes();
+		System.out.println(timer.timeElapsed() + "ms");
+		System.out.println(primes.get(limit - 1));
+	}
+	
+	private static void calculatePrimes() {
+		int startingNumber = 0;
+		primes.add(2);
+		primes.add(3);
+		for(int i = 1; i < limit; i++) {
+			startingNumber = primes.get(i) + 2;
+			boolean primeNotFound = true;
+			for(int prime = startingNumber; primeNotFound; prime++) {
+				if(isPrime(prime)) {
+					primes.add(prime);
+					primeNotFound = false;
+				}
+			}
+		}
+			
+	}
+	
+	private static boolean isPrime(int prime) {
+		for(int divisor = prime / 2; divisor > 1; divisor--)
+			if(prime % divisor == 0)
+				return false;
+		return true;
+	}
+	
+}
---
+++ src/problems/Problem016.java
@@ -0,0 +1,19 @@
+package problems;
+
+import java.math.BigInteger;
+
+// solved
+
+public class Problem016 {
+	
+	public static void main(String[] args) {
+		BigInteger number = new BigInteger("2").pow(1000);
+		String string = number.toString();
+		System.out.println(string);
+		long sum = 0;
+		for(int i = 0; i < string.length(); i++)
+			sum += Integer.parseInt(string.substring(i, i + 1));
+		System.out.println(sum);
+	}
+
+}
