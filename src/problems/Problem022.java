49c80fb694ce70e1535d54a9f12fce42fde15833
Joel Niklaus <me@joelniklaus.ch>
2014-07-18 00:11:24 +0200

    Problem 022 solved

--- src/problems/Problem022.java
+++ src/problems/Problem022.java
@@ -7,50 +7,54 @@ import java.nio.file.Paths;
 
 // solved
 
-public class Problem042 {
+public class Problem022 {
 
-	static int numberOfTriangles = 100;
-	static int[] triangles = new int[numberOfTriangles];
-	static int sum = 0;
-	static String[] words = new String[4];
-	
+	static long sum = 0;
+	static String[] names;
+
 	public static void main(String[] args) throws IOException {
-		computeTriangles();
-		for(int triangle : triangles)
-			System.out.println(triangle);
-		String wordString = readFile("/Users/joelniklaus/Google Drive/Privat/Informatik/Java/ProjectEuler/src/problems/Files/words.txt", Charset.defaultCharset());
-		splitWordString(wordString);
-		for(String word : words) {
-			System.out.println(word);
-			if(isTriangleWord(word))
-				sum++;
-		}
+		String nameString = readFile("/Users/joelniklaus/Google Drive/Privat/Informatik/Java/ProjectEuler/src/problems/Files/names.txt", Charset.defaultCharset());
+		splitWordString(nameString);
+		names = sort(names);
+		
+		for(int i = 0; i < names.length; i++)
+			sum += score(i);
 		System.out.println(sum);
 	}
-	
-	private static void computeTriangles() {
-		for(int i = 1; i <= numberOfTriangles; i++)
-			triangles[i - 1] = i * (i + 1) / 2;
-	}
-	
-	private static void splitWordString(String wordString) {
-		words = wordString.split(",");
-		for(int i = 0; i < words.length; i++)
-			words[i] = words[i].substring(1, words[i].length() - 1);
-	}
 
-	static String readFile(String path, Charset encoding) throws IOException {
+	private static String readFile(String path, Charset encoding) throws IOException {
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
 		return new String(encoded, encoding);
 	}
+	
+	private static void splitWordString(String nameString) {
+		names = nameString.split(",");
+		for(int i = 0; i < names.length; i++)
+			names[i] = names[i].substring(1, names[i].length() - 1);
+	}
 
-	private static boolean isTriangleWord(String word) {
+	private static int score(int index) {
 		int score = 0;
-		for(char character : word.toCharArray())
+		for(char character : names[index].toCharArray())
 			score += (int) character - 64;
-		for(int triangle : triangles)
-			if(score == triangle)
-				return true;
-		return false;
+		score *= index + 1;
+		return score;
+	}
+
+	private static String[] sort(String[] array) {
+		String tmp;
+		for (int i = 0;i < array.length;i++) {
+			tmp = array[i];
+			for (int j = 0;j < array.length;j++) {
+				if (i == j) continue;
+				int x = tmp.compareTo(array[j]);
+				if (x < 0) {
+					tmp = array[j];
+					array[j] = array[i];
+					array[i] = tmp;
+				}
+			}
+		}
+		return array;
 	}
 }
