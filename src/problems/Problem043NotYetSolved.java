f25c40501f0ddb6897ae7a23b9c533f212e0601a
Joel Niklaus <me@joelniklaus.ch>
2014-07-18 12:23:44 +0200

    Various new Problems solved and started

---
+++ src/problems/Problem043NotYetSolved.java
@@ -0,0 +1,16 @@
+package problems;
+
+import java.util.ArrayList;
+
+import problems.Utilities.Primes;
+
+
+public class Problem043NotYetSolved {
+
+	static ArrayList<Integer> primes = Primes.getAllPrimes(18);
+	
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/problems/Utilities/Primes.java
+++ src/problems/Utilities/Primes.java
@@ -32,11 +32,44 @@ public class Primes {
 		return primes;
 	}
 	
+	/**
+	 * Returns all Primes smaller than the limit.
+	 */
 	public static ArrayList<Integer> getAllPrimes(int limit) {
 		return getPrimes(2, limit);
 	}
 
+	/**
+	 * Returns all Primes smaller than the limit except 2.
+	 */
 	public static ArrayList<Integer> getOddPrimes(int limit) {
 		return getPrimes(3, limit);
+	}
+
+	/**
+	 * Generates a list with all the prime factors of the number.
+	 * 
+	 * @param number	of which we want to know the prime factors
+	 * @param primes	the prime factors we want to check for
+	 * @return			an ArrayList of Integers with the prime factors
+	 */
+	public static ArrayList<Integer> getPrimeFactors(int number, ArrayList<Integer> primes) {
+		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+		for(int prime : primes) {
+			if(number == 1)
+				break;
+			number = divide(number, primeFactors, prime);
+		}
+		return primeFactors;	
+	}
+
+	private static int divide(int number, ArrayList<Integer> primeFactors, int prime) {
+		if(number % prime == 0) {
+			number /= prime;
+			if(!primeFactors.contains(prime))
+				primeFactors.add(prime);
+			return divide(number, primeFactors, prime);
+		}
+		return number;
 	}
 }
