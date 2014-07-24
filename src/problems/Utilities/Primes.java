9c4f061d7254d441de62157828ca5f8ced440808
Joel Niklaus <me@joelniklaus.ch>
2014-07-24 22:26:18 +0200

    new Problems added

--- src/problems/Utilities/Primes.java
+++ src/problems/Utilities/Primes.java
@@ -3,73 +3,75 @@ package problems.Utilities;
 import java.util.ArrayList;
 
 public class Primes {
-
-	private static boolean[] numbers;
 	
-	private static void calculatePrimes(int limit) {
-		numbers = new boolean[limit + 1];
+	private static boolean[] numbers;
+
+	private static boolean[] calculatePrimes(int limit) {
+		boolean[] numbers = new boolean[limit + 1];
 		numbers[0] = true;
 		numbers[1] = true;
-		for(int prime = 2; prime <= limit; prime++)
-			if(!numbers[prime])
-				for(int multiple = 2*prime; multiple <= limit; multiple += prime)
+		for (int prime = 2; prime <= limit; prime++)
+			if (!numbers[prime])
+				for (int multiple = 2 * prime; multiple <= limit; multiple += prime)
 					numbers[multiple] = true;
+		return numbers;
 	}
-	
-	/**
-	 * true means not prime. false means prime.
-	 */
+
+	// true means not prime. false means prime.
 	private static boolean isPrime(int number) {
 		return !numbers[number];
 	}
-	
+
 	private static ArrayList<Integer> getPrimes(int start, int limit) {
-		calculatePrimes(limit);
+		numbers = calculatePrimes(limit);
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		for(int i = start; i < limit; i++)
-			if(isPrime(i))
+		for (int i = start; i < limit; i++)
+			if (isPrime(i))
 				primes.add(i);
 		return primes;
 	}
-	
+
 	/**
 	 * Returns all Primes smaller than the limit.
 	 */
 	public static ArrayList<Integer> getAllPrimes(int limit) {
 		return getPrimes(2, limit);
 	}
-
+	
 	/**
 	 * Returns all Primes smaller than the limit except 2.
 	 */
 	public static ArrayList<Integer> getOddPrimes(int limit) {
 		return getPrimes(3, limit);
 	}
-
+	
 	/**
 	 * Generates a list with all the prime factors of the number.
-	 * 
-	 * @param number	of which we want to know the prime factors
-	 * @param primes	the prime factors we want to check for
-	 * @return			an ArrayList of Integers with the prime factors
+	 *
+	 * @param number
+	 *            of which we want to know the prime factors
+	 * @param primes
+	 *            the prime factors we want to check for
+	 * @return an ArrayList of Integers with the prime factors
 	 */
 	public static ArrayList<Integer> getPrimeFactors(int number, ArrayList<Integer> primes) {
 		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
-		for(int prime : primes) {
-			if(number == 1)
+		for (int prime : primes) {
+			if (number == 1)
 				break;
 			number = divide(number, primeFactors, prime);
 		}
-		return primeFactors;	
+		return primeFactors;
 	}
 
 	private static int divide(int number, ArrayList<Integer> primeFactors, int prime) {
-		if(number % prime == 0) {
+		if (number % prime == 0) {
 			number /= prime;
-			if(!primeFactors.contains(prime))
+			if (!primeFactors.contains(prime))
 				primeFactors.add(prime);
 			return divide(number, primeFactors, prime);
 		}
 		return number;
 	}
+
 }
