8962086b291375065805dfaf66805f1d99b041e3
Joel Niklaus <me@joelniklaus.ch>
2014-10-19 17:19:00 +0200

    new Problems solved

--- src/problems/until49/Problem017Awkward.java
+++ src/problems/until49/Problem017Awkward.java
@@ -1,6 +1,8 @@
 package problems.until49;
 
-public class Problem017NotYetSolved {
+public class Problem017Awkward {
+	
+	static int correctSolution = 21124; // ?????
 	
 	static int limit = 1001;
 	// contains length of numbers
@@ -12,22 +14,29 @@ public class Problem017NotYetSolved {
 		init();
 		buildNumbers();
 		int sum = 0;
-		for (int number : numbers)
+		int i = 0;
+		for (int number : numbers) {
 			sum += number;
+			// if (i == 9 || i == 19 || i == 99 || i == 999)
+			// System.out.println(sum);
+			System.out.println(i + ".: " + number);
+			i++;
+		}
 		System.out.println(sum);
 	}
 	
 	private static void buildNumbers() {
-		// fill all numbers 21 - 100
+		// fill all numbers 21 - 99
 		for (int i = 21; i < 100; i++)
 			numbers[i] = numbers[getTener(i)] + numbers[getOner(i)];
 		// fill even hundreds
 		for (int i = 100; i < 1000; i += 100)
 			numbers[i] = numbers[i / 100] + hundred;
-		// fill all numbers 100 - 1000
+		// fill all numbers 201 - 999 except even hundreds
 		for (int i = 101; i < 1000; i++)
-			numbers[i] = numbers[getHundreder(i)] + and + numbers[getTener(i)]
-					+ numbers[getOner(i)];
+			if (i % 100 != 0)
+				numbers[i] = numbers[getHundreder(i)] + and + numbers[getTener(i)]
+						+ numbers[getOner(i)];
 	}
 	
 	private static int getOner(int number) {
@@ -64,15 +73,15 @@ public class Problem017NotYetSolved {
 		numbers[15] = 7;
 		numbers[16] = 7;
 		numbers[17] = 9;
-		numbers[18] = 9;
+		numbers[18] = 8;
 		numbers[19] = 8;
 		numbers[20] = 6;
 		numbers[30] = 6;
-		numbers[40] = 6;
+		numbers[40] = 5;
 		numbers[50] = 5;
 		numbers[60] = 5;
 		numbers[70] = 7;
-		numbers[80] = 7;
+		numbers[80] = 6;
 		numbers[90] = 6;
 		numbers[1000] = 11;
 	}
--- src/problems/until49/Problem032.java
+++ src/problems/until49/Problem032.java
@@ -2,46 +2,48 @@ package problems.until49;
 
 import java.util.ArrayList;
 
+// solved
 
-public class Problem032NotYetSolved {
+public class Problem032 {
 	
 	static ArrayList<Integer> pandigitals = new ArrayList<Integer>();
 	static int sum;
 
-	public static void main(String[] args) {		
-		for(int multiplicand = 2; multiplicand < 987; multiplicand++)
-			for(int multiplier = 2; multiplier < 987; multiplier++) {
+	public static void main(String[] args) {
+		for (int multiplicand = 2; multiplicand < 9876; multiplicand++)
+			for (int multiplier = 2; multiplier < 9876; multiplier++) {
 				int product = multiplicand * multiplier;
-				if(isPandigital(multiplicand, multiplier, product) && !pandigitals.contains(product)) {
+				if (isPandigital(multiplicand, multiplier, product)
+						&& !pandigitals.contains(product)) {
 					System.out.println(multiplicand + " * " + multiplier + " = " + product);
 					pandigitals.add(product);
-				}	
+				}
 			}
 		
-		for(int pandigital : pandigitals)
+		for (int pandigital : pandigitals)
 			sum += pandigital;
 		System.out.println(sum);
 	}
 
 	private static boolean isPandigital(int multiplicand, int multiplier, int product) {
 		String numberString = multiplicand + "" + multiplier + "" + product;
-		if(numberString.length() > 9)
+		if (numberString.length() > 9)
 			return false;
-		if(numberString.contains("0"))
+		if (numberString.contains("0"))
 			return false;
 		int number = Integer.parseInt(numberString);
 		boolean[] digits = new boolean[9];
 		// add the digits
 		while (number > 0) {
 			int digit = number % 10 - 1;
-			if(digits[digit] == true)
+			if (digits[digit] == true)
 				return false;
 			digits[digit] = true;
-			number = number / 10;
+			number /= 10;
 		}
 		// check if there are all nine digits used
-		for(int i = 0; i < digits.length; i++)
-			if(digits[i] == false)
+		for (int i = 0; i < digits.length; i++)
+			if (digits[i] == false)
 				return false;
 		return true;
 	}
